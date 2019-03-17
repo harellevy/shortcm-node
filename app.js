0706494f33c4b051d7b8393ae0d35bc7309c9e17
Michael Dunaevsky <duna87@gmail.com>
2019-03-17 09:55:16 +0200

    .

--- app.js
+++ app.js
@@ -109,8 +109,8 @@ const shortCm = {
 
     expand: (url) => {
         return new Promise((resolve, reject) => {
-            let pathname = URL.parse(url).pathname;
-            pathname = encodeURI(stripLeadingSlash(stripTrailingSlash(pathname)));
+            const myURL = new URL(url);
+            const pathname = encodeURI(stripLeadingSlash(stripTrailingSlash(myURL.pathname)));
             const short = encodeURI(shortDomain);
 
             const options = {
